4920903c58983bf826c761bffd4411a10fd16fd5
Madalyn Elliott <143676338+melliott3@users.noreply.github.com>
2023-11-14 02:09:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ function init() {
 function showMeText() {
   var textbox = document.getElementById('entryinput');
   document.getElementById('textoutput').innerHTML = textbox.value;
-  alert("Daniel Krieglstein: " + textbox.value);
+  customAlert("Daniel Krieglstein: " + textbox.value);
 }
+
   button.addEventListener('click', showMeText);
-  window.addEventListener('load', init);
 }
+
+function customAlert(message) {
+  alert(message);
+}
+
+  window.addEventListener('load', init);
 
 /*function init(){
   var button = document.getElementById('entrybutton');
