cd50f1d62f39e2412acb772a8ea3eebdbee92f32
Madalyn Elliott <143676338+melliott3@users.noreply.github.com>
2023-11-14 01:36:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 function init() {
-  var button = document.getElementById("entrybutton");
+  var button = document.getElementById('entrybutton');
 
 function showMeText() {
-  var textbox = document.getElementById("entryinput");
-  document.getElementById("textoutput").innerHTML = textbox.value;
+  var textbox = document.getElementById('entryinput');
+  document.getElementById('textoutput').innerHTML = textbox.value;
   alert("Daniel Krieglstein: " + textbox.value);
 }
   button.addEventListener('click', showMeText);
-}
   window.addEventListener('load', init);
+}
+
+/*function init(){
+  var button = document.getElementById('entrybutton');
+
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Daniel Krieglstein: " + textbox.value);
+  }
+
+  button.addEventListener('click', showMeText);
+  }
+  
+window.addEventListener('load', init);*/
