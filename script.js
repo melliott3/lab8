dc6f9de93025c7a36a1e2d80fa706f434b7f5753
Madalyn Elliott <143676338+melliott3@users.noreply.github.com>
2023-11-14 00:43:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function init(){
   }
   
 window.addEventListener('load', init);
+
+document.getElementById('alertMe').addEventListener('click', function() {
+      alert('Daniel Krieglstein: ' + document.getElementById('textInput').value);
+    });
+
+    // Add event listener to the submit button
+    document.getElementById('submitButton').addEventListener('click', function() {
+      // Get the value of the text input
+      var inputValue = document.getElementById('textInput').value;
+
+      // Display an alert with name and colon before the input value
+      alert('Daniel Krieglstein: ' + inputValue);
+
+      // Set the text content of the h2 element to the input value
+      document.getElementById('outputText').textContent = 'Output: ' + inputValue;
+    });
